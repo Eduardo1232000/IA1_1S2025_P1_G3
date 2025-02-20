94c14b07c5eb101370ce5994f2e1876057a1de6e
Eduardo Reyes <eduardoalex2000@hotmail.com>
2025-02-20 10:48:36 -0600

    [ADD] VALIDACION CODIGO PROLOG
    Esta funcion valida si hay en el session storage codigo de prolog

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,43 @@
+let session = pl.create(1000);
+let CodigoProlog = "";
+window.onload = function () {
+    validar_codigo_prolog();
+};
+function validar_codigo_prolog() {
+    try {
+        CodigoProlog = sessionStorage.getItem("CODIGO_PROLOG");
+        (CodigoProlog === null)
+            ? (console.log("No hay codigo Almacenado!"))
+            : (console.log("HAY CODIGO ALMACENADO"), console.log(CodigoProlog),document.getElementById("STATUS_PROLOG").style.backgroundColor = "green");
+    } catch (error) {
+        console.log("ERROR AL OBTENER CODIGO PROLOG")
+        console.log(error)
+    }
+}
+
+document.getElementById('fileInput').addEventListener('change', function (event) {
+    const file = event.target.files[0];
+    if (!file) return;
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        CodigoProlog = e.target.result;
+        session = pl.create(1000);
+        session.consult(CodigoProlog, {
+            success: function () {
+                console.log("EXITO AL CARGAR")
+                console.log(CodigoProlog)
+                document.getElementById("STATUS_PROLOG").style.backgroundColor = "green"
+                sessionStorage.setItem("CODIGO_PROLOG", CodigoProlog);
+            },
+            error: function (err) {
+                console.log("Error al cargar el código: " + err);
+            }
+        });
+    };
+    reader.readAsText(file);
+});
+
+
 function AccionSiguiente() {
     alert("No Implementado")
 }
--- js/analisis_carrera.js
+++ js/analisis_carrera.js
@@ -1,3 +1,20 @@
+let session = pl.create(1000);
+let CodigoProlog = "";
+window.onload = function () {
+    validar_codigo_prolog();
+};
+
+function validar_codigo_prolog() {
+    try {
+        CodigoProlog = sessionStorage.getItem("CODIGO_PROLOG");
+        (CodigoProlog === null)
+            ? (console.log("No hay codigo Almacenado!"))
+            : (console.log("HAY CODIGO ALMACENADO"), console.log(CodigoProlog));
+    } catch (error) {
+        console.log("ERROR AL OBTENER CODIGO PROLOG")
+        console.log(error)
+    }
+}
 function avanzar_1() {
     document.getElementById("ESTUDIANTE_DIV_PARTE1").classList.add("oculto");
     document.getElementById("ESTUDIANTE_DIV_PARTE2").classList.remove("oculto");
@@ -18,4 +35,5 @@ function regresar_2() {
 function agregar() {
     alert("No implementado")
 }
+
 
